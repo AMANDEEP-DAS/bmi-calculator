eac472c4fb744fbb839206f980c70c8d2bf37c66
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-09 12:52:18 +0530

    color change

--- lib/screens/container.dart
+++ lib/screens/container.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 
 class BoxContainers extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width/2.2,
+      width: MediaQuery.of(context).size.width / 2.2,
       decoration: BoxDecoration(
-        color: Colors.red.shade900,
-        borderRadius: BorderRadius.all(Radius.circular(30))
-      ),
+          color: Colors.grey.shade900,
+          borderRadius: BorderRadius.all(Radius.circular(30))),
     );
   }
 }
-
